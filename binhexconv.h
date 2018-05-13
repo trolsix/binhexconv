2cac39d180cf5387b6a89c633bdbc2dd13d2adf1
trolsix <39171229+trolsix@users.noreply.github.com>
2018-05-13 12:59:41 +0000

    Create binhexconv.h

---
+++ binhexconv.h
@@ -0,0 +1,23 @@
+
+
+#ifndef _BINHEXCONV_H
+#define _BINHEXCONV_H
+
+#define _HEXOP_VER 002
+#define SIZCHAR 0
+
+#if SIZCHAR==0
+void b2ah (char * str, void * bin , unsigned int size );
+void ah2b ( void * bin, char * str, unsigned int size );
+void * sah2b ( void * bin, char * str, unsigned int size );
+#else
+void b2ah (char * str, void * bin , unsigned char size );
+void ah2b ( void * bin, char * str, unsigned char size );
+void * sah2b ( void * bin, char * str, unsigned char size );
+#endif
+
+#define BINTOASCHEX(TBL,LI) b2ah(TBL,(void*)&LI,sizeof(LI))
+#define ASCHEXTOBIN(LI,TBL) ah2b((void*)&LI,TBL,sizeof(LI))
+#define STRASCHEXTOBIN(LI,TBL) sah2b((void*)&LI,TBL,sizeof(LI))
+
+#endif
