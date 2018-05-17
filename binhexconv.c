b5d4d35a11e578760ef3eebebc393c39aba89a1f
trolsix <39171229+trolsix@users.noreply.github.com>
2018-05-17 12:49:56 +0000

    Update binhexconv.c

--- binhexconv.c
+++ binhexconv.c
@@ -4,22 +4,16 @@ author: Tomasz C.
 trol.six www.elektroda.pl
 aktyn www.gentoo.org
 
------------------------------------------------*/
-/*-----------------------------------------------
 MIT License
-
 Copyright (c) 2018 trolsix
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,25 +23,23 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 -----------------------------------------------*/
 /*-----------------------------------------------
-
 conversion between:
 binary -> ascii hex
 ascii hex -> binary
 ascii stream -> binary
   
 for little endian (or big endian with correct length)
-
 -----------------------------------------------*/
 
 #include "binhexconv.h"
 
-#if SIZCHAR==0
-void b2ah (char * str, void * bin , unsigned int size) {
-#else
-void b2ah (char * str, void * bin , unsigned char size) {
-#endif
+#if _USE_BINTOASCHEX
+
+void b2ah (char * str, void * bin , WORKSIZ size) {
 	unsigned char tmp, b;
-	unsigned char * lb = bin;
+	unsigned char * lb;
+	
+	lb = (unsigned char *) bin;
 	
 	while(size--){
 		b = lb[size];
@@ -63,89 +55,95 @@ void b2ah (char * str, void * bin , unsigned char size) {
 	*str = 0;
 }
 
+#endif
+
+/*-----------------------------------------------
+ 
+-----------------------------------------------*/
+
+#if _USE_STRASCHEXTOBIN
+static unsigned char hashexdigit ( unsigned char tmp2 ) {
+	if ( ( tmp2 >= '0' ) && ( tmp2 <= '9' ) ) return tmp2 - ('0'-0);
+	if ( ( tmp2 >= 'a' ) && ( tmp2 <= 'f' ) ) return tmp2 - ('a'-10);
+	if ( ( tmp2 >= 'A' ) && ( tmp2 <= 'F' ) ) return tmp2 - ('A'-10);
+	return 0x80;
+}
+#else
+static inline unsigned char hashexdigit ( unsigned char tmp2 ) {	
+	if (tmp2 >= 'a') tmp2 -= 'a' - 'A';
+	if (tmp2 >= 'A') tmp2 -= 'A' - '9' - 1;
+	tmp2 -= '0';
+	return tmp2;	
+}
+#endif
+
 /*-----------------------------------------------
  read ascii number with end of string
 -----------------------------------------------*/
 
-static inline unsigned char hashexdigit ( unsigned char tmp2 ) {
-	if ( ( tmp2 >= '0' ) && ( tmp2 <= '9' ) ) return tmp2 - ('0'-1);
-	if ( ( tmp2 >= 'a' ) && ( tmp2 <= 'f' ) ) return tmp2 - ('a'-11);
-	if ( ( tmp2 >= 'A' ) && ( tmp2 <= 'F' ) ) return tmp2 - ('A'-11);
-	return 0;
-}
+#if _USE_ASCHEXTOBIN && _MAX_BYTES>4
+#if !(_MERGE_IF_POSSIBILE && _USE_STRASCHEXTOBIN)
 
-#if SIZCHAR==0
-void ah2b ( void * bin, char * str, unsigned int size ) {
-#else
-void ah2b ( void * bin, char * str, unsigned char size ) {
-#endif
-	unsigned char tmp, tmp2, tmp3;
-	unsigned char * lb = bin;
+void ah2b ( void * bin, const char * str, WORKSIZ size ) {
+	WORKSIZ lstr;
+	unsigned char tmp3;
+	unsigned char *lb;
 
-	tmp = 0;
+	lb = (unsigned char*) bin;
+	lstr = 0;
 	tmp3 = 0;
 	
-	while(*str!=0) {
-		++str;
-		++tmp;
-	}
-
-	while(size) {
-		if ( tmp == 0 ) break;	
-		
-		tmp2 = *--str;
-		tmp3 = hashexdigit(tmp2) - 1;
-
-		if ( --tmp == 0 ) {
-			*lb++ = tmp3;
-			--size;
-			break;
-		} else {
-			tmp2 = *--str;
-			tmp2 = hashexdigit(tmp2) - 1;
-		}
-		
-		tmp2 <<= 4;
-		*lb++ = tmp3 | tmp2;
-		--tmp;
+	while ( *str != 0 ) { ++str; ++lstr; }
+	
+	while ( size && lstr-- ) {		
+		tmp3 = hashexdigit(*--str);
 		--size;
-	}
 
-	while(size--) {
-		*lb++ = 0;
+		if ( lstr == 0 ) {
+			*lb++ = tmp3;
+			break;
+		}
+		tmp3 |= hashexdigit(*--str) << 4;
+		*lb++ = tmp3;
+		--lstr;
 	}
+	
+	while (size--) *lb++ = 0;
 }
+#endif
+#endif
 
 /*-----------------------------------------------
+find hex digit in the string and
 read ascii number from string and return:
 pointer after at the places of number
 or:
-0 - find no number
+0 (null pointer) - find no number
 -----------------------------------------------*/
 
-#if SIZCHAR==0
-void * sah2b ( void * bin, char * str, unsigned int size ) {
-#else
-void * sah2b ( void * bin, char * str, unsigned char size ) {
-#endif
-	unsigned char tmp, tmp2, tmp3;
-	unsigned char * lb = bin;
-	
-	tmp = 0;
+#if _USE_STRASCHEXTOBIN && _MAX_BYTES>4
+
+void * sah2b ( void * bin, const char * str, WORKSIZ size ) {
+	WORKSIZ lstr;
+	unsigned char tmp3;
+	unsigned char * lb;
+
+	lb = (unsigned char*) bin;
+	lstr = 0;
 	tmp3 = 0;
 
 	if( !str ) return 0;
 
 	while (1) {
-		tmp2 = *str;
-		if( !tmp2 ) return (void*)0;
-		if ( hashexdigit(tmp2) ) break;
+		tmp3 = *str;
+		if ( !tmp3 ) return (void*)0;
+		if ( (hashexdigit(tmp3) & 0x80) == 0 ) break;
 		++str;
 	}
 	
-	for ( tmp=0; tmp2; ++tmp, ++str ) {
-		tmp2 = *str;
-		if ( 0 == hashexdigit(tmp2) ) break;
+	for ( lstr=0; tmp3; ++lstr, ++str ) {
+		tmp3 = *str;
+		if ( hashexdigit(tmp3) & 0x80 ) break;
 	}
 	
 	if (*str==0) bin = (void*)0;
@@ -153,33 +151,83 @@ void * sah2b ( void * bin, char * str, unsigned char size ) {
 	
 	tmp3 = 0;
 
-	while (size&&tmp) {
-		if ( tmp == 0 ) break;
-
-		tmp2 = *--str;
-		tmp3 = hashexdigit(tmp2) - 1;
-	
-		if ( --tmp == 0 ) {
-			*lb++ = tmp3;
-			--size;
-			break;
-		} else {
-			tmp2 = *--str;
-			tmp2 = hashexdigit(tmp2) - 1;
-		}
-		
-		tmp2 <<= 4;
-		*lb++ = tmp3 | tmp2;
-		--tmp;
+	while ( size && lstr-- ) {
+		tmp3 = hashexdigit(*--str);
 		--size;
+		
+		if ( lstr == 0 ) {
+			*lb++ = tmp3;
+			break;
+		}
+		tmp3 |= hashexdigit(*--str) << 4;
+		*lb++ = tmp3;
+		--lstr;
 	}
 
-	while (size--) {
-		*lb++ = 0;
-	}
+	while (size--) *lb++ = 0;
+	
 	return bin;
 }
 
+#endif
+
+/*-----------------------------------------------
+ only max 4 bytes
+-----------------------------------------------*/
+
+#if _MAX_BYTES<=4
+
+#include <stdint.h>
+
+#if _USE_ASCHEXTOBIN && !(_MERGE_IF_POSSIBILE && _USE_STRASCHEXTOBIN)
+
+void ah2b ( void * bin, const char * str, WORKSIZ size ) {
+	unsigned char tmp3, *lb;
+
+	union {
+		uint8_t tn[4];
+		uint32_t t32;
+	} ttt;
+
+	if ( !str ) return;
+
+	while ( 0 != (tmp3 = *str++ ) ) {
+		tmp3 = hashexdigit(tmp3);
+		ttt.t32 = ttt.t32 << 4 | tmp3;
+	}
+	
+	lb = (unsigned char*) bin;
+	for ( tmp3=0; tmp3<size; tmp3++ ) lb[tmp3] = ttt.tn[tmp3];
+}
+#endif
+
+#if _USE_STRASCHEXTOBIN
+
+void * sah2b ( void * bin, const char * str, WORKSIZ size ) {
+	unsigned char tmp3, *lb;
+
+	union {
+		uint8_t tn[4];
+		uint32_t t32;
+	} ttt;
+
+	if ( !str ) return (void*)0;
+
+	while (1) {
+		tmp3 = hashexdigit(*str++);
+		if ( 0x80 & tmp3 ) break;
+		ttt.t32 = ttt.t32 << 4 | tmp3;
+	}
+
+	lb = (unsigned char*)bin;
+	for ( tmp3=0; tmp3<size; tmp3++ ) lb[tmp3] = ttt.tn[tmp3];
+
+	return str;
+}
+
+#endif
+
+#endif
 
 /*-----------------------------------------------
 
